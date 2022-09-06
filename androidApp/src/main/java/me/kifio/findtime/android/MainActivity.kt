6e1cf9d69059a83942a39809cce7979956a4b6d0
kifio <kg.of.morphine@gmail.com>
2022-09-07 00:42:29 +0300

    Start implement UI for Android

--- androidApp/src/main/java/me/kifio/findtime/android/MainActivity.kt
+++ androidApp/src/main/java/me/kifio/findtime/android/MainActivity.kt
@@ -2,11 +2,31 @@ package me.kifio.findtime.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import me.kifio.findtime.android.ui.MainView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainView {
+                TopAppBar(title = {
+                    when (it) {
+                        0 -> Text(text = stringResource(R.string.world_clocks))
+                        else -> Text(text = stringResource(R.string.findmeeting))
+                    }
+                })
+            }
+        }
     }
+}
+
+@Composable
+fun showName(text: String) {
+    Text(text)
 }
---
+++ androidApp/src/main/java/me/kifio/findtime/android/ui/MainView.kt
@@ -0,0 +1,89 @@
+package me.kifio.findtime.android.ui
+
+import android.app.ActionBar
+import android.widget.ImageView
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import me.kifio.findtime.android.theme.AppTheme
+import me.kifio.findtime.android.ui.BottomNavigationItem
+
+sealed class Screen(val title: String) {
+    object TimeZoneScreen : Screen("Timezones")
+    object FindTimeScreen : Screen("Find Time")
+}
+
+data class BottomNavigationItem(
+    val route: String,
+    val icon: ImageVector,
+    val iconContentDescription: String
+)
+
+val bottomNavigationItems = listOf(
+    BottomNavigationItem(
+        Screen.TimeZoneScreen.title,
+        Icons.Filled.Language,
+        "Timezones"
+    ),
+    BottomNavigationItem(
+        Screen.FindTimeScreen.title,
+        Icons.Filled.Place,
+        "Find Time"
+    )
+)
+
+@Composable
+fun MainView(actionBar: topBarFun = { emptyComposable() }) {
+    val showAddDialog = remember { mutableStateOf(false) }
+    val currentTimezoneStrings = remember { SnapshotStateList<String>() }
+    val selectedIndex = remember { mutableStateOf(0) }
+
+    AppTheme {
+        Scaffold(
+            topBar = {
+                actionBar(selectedIndex.value)
+            },
+            floatingActionButton = {
+                if (selectedIndex.value == 0) {
+                    FloatingActionButton(
+                        modifier = Modifier.padding(16.dp),
+                        onClick = { showAddDialog.value = true }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null
+                        )
+                    }
+                }
+            },
+            bottomBar = {
+                BottomNavigation {
+                    bottomNavigationItems.forEachIndexed { index, bottomNavigationItem ->
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(
+                                    imageVector = bottomNavigationItem.icon,
+                                    contentDescription = bottomNavigationItem.iconContentDescription
+                                )
+                            },
+                            selected = selectedIndex.value == index,
+                            onClick = { selectedIndex.value = index }
+                        )
+                    }
+                }
+            }
+        ) {
+
+        }
+    }
+}
